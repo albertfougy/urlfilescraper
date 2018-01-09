81398254760753ae4b010fd2845a0958e5341964
Albert Fougy <albert@fougy.com>
2018-01-09 08:41:57 -0500

    Reformatted function.Removed bloated comments. Fixed problem with reading urls.txt

--- url_file_scraper.rb
+++ url_file_scraper.rb
@@ -28,11 +28,6 @@ def get_xml_url(url)
   result_url.gsub('result', 'xmlResult')
 end
 
-##########################################################
-# Create a function in this section?
-# Pass an argument here to the XML Parser from Mail
-# THIS WILL BE THE MAIN CONTROL STATEMENT
-#########################################################
 
 # You need to MATCH SPECIFIC NODE NAME and NODE TEXT
 # The fields that I need to extract the data
@@ -63,6 +58,7 @@ end
 #   return test_url
 # end
 
+
 def run
   all_results = {}
   web_results = ""
@@ -70,16 +66,15 @@ def run
   File.open('urls.txt','r') do |file_handle|
     file_handle.each_line do |line|
       xml_url = get_xml_url(line)
-      host = URI.parse(line.strip).host.downcase # need to refactor for malform links
+      host = URI.parse(line.strip).host.downcase
       all_results[host] = return_results(xml_url)
-      all_results.each do |key, value|
-        value.each do |attri, info|
-          puts "#{key.chomp}: The #{attri} result is: #{info}"
-          web_results += "#{key.chomp}: The #{attri} result is: #{info}\n"
-# "#{line_num += 1} #{line}"
-        end
-      end
-     web_results
+    end
+  end
+
+  all_results.each do |key, value|
+    value.each do |attri, info|
+      puts "#{key.chomp}: The #{attri} result is: #{info}"
+      web_results += "#{key.chomp}: The #{attri} result is: #{info}\n"
     end
   end
 
@@ -90,38 +85,19 @@ def run
   email.deliver
 end
 
-# Results by console and email
 
-# google.com: Load Time is 955
-# google.com: First Byte is 552
-# google.com: Start Render is 1178
-# google.com: Speed Index is 1470
-# google.com: DOM Elements is 375
-# google.com: Time is 2263
-# www.techcrunch.com: Load Time is 10698
-# www.techcrunch.com: First Byte is 670
-# www.techcrunch.com: Start Render is 8790
-# www.techcrunch.com: Speed Index is 12270
-# www.techcrunch.com: DOM Elements is 3556
-# www.techcrunch.com: Time is 28637
-# albertfougy.com: Load Time is 3818
-# albertfougy.com: First Byte is 319
-# albertfougy.com: Start Render is 1391
-# albertfougy.com: Speed Index is 1576
-# albertfougy.com: DOM Elements is 597
-# albertfougy.com: Time is 3895
-# codebuddies.org: Load Time is 4189
-# codebuddies.org: First Byte is 1087
-# codebuddies.org: Start Render is 4570
-# codebuddies.org: Speed Index is 4684
-# codebuddies.org: DOM Elements is 360
-# codebuddies.org: Time is 16632
 
-# Pretty printed email send results to console
 
-# => #<Mail::Message:70334122941920, Multipart: false, Headers: <Date: Fri, 22 Dec 2017 20:27:25 -0500>,
-# <From: myemailaddress>, <To: recipient's email address >, <Message-ID:
-# <gobblygook@my-iMac.fios-router.home.mail>>,
-# <Subject: email trail run of hashes>, <Mime-Version: 1.0>, <Content-Type: text/plain>,
-# <Content-Transfer-Encoding: 7bit>>
+
+# stain88 [6:40 AM]
+# i think it might just be because you’re printing `all_results.each` inside the `File.open` block,
+# so when it’s reading url 2, it prints url 1 + 2, when reading url 3, it prints url 1 + 2 + 3, etc
+
+
+# [6:40]
+# which kinda “adds up” to your output
+
+
+# [6:41]
+# you can just do that printing loop after you end the `File.open` block
 
